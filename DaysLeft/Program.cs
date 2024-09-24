a96f3d2093431da56f18e1c883011dfb64233d44
RyanPSN <ryangregoire286@gmail.com>
2024-09-24 13:27:29 -0400

    Add project files.

---
+++ DaysLeft/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DaysLeft
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            var month = 0;
+            var passedMonth = 0;
+            var bDate = 0;
+            var currentDay = 0;
+
+            Console.WriteLine("Welcome Birthday Date Calculation Application");
+            Console.WriteLine(" ");
+
+            Console.Write("Enter Current Month: ");
+            month += int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Birthday Month: ");
+            passedMonth += int.Parse(Console.ReadLine());
+
+            Console.Write("Enter B Day: ");
+            bDate += int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Current Day: ");
+            currentDay += int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your Birthday is in: {0} Month's", 
+                (Math.Abs(passedMonth - month)));
+            Console.WriteLine("Your Birthday is in: {0} Day's ",
+                (Math.Abs(currentDay - bDate - 30)));
+
+            Console.ReadLine();
+        }
+
+    }
+}
